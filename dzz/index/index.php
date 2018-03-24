a421aae2ceeefa97889c9f78d6a8c6ad20e062bb
zyx0814 <zyx0814@163.com>
2018-03-24 19:28:51 +0800

    index redirect

--- dzz/index/index.php
+++ dzz/index/index.php
@@ -1,7 +1,7 @@
 <?php
 /*默认页跳转*/
 
-$app=DB::fetch_first("select * from %t where `group`<3 and isshow>0 and `available`>0 ORDER BY disp",array('app_market'));
+$app=DB::fetch_first("select * from %t where (`group`<3 ||identifier='appmanagement') and isshow>0 and `available`>0 ORDER BY disp",array('app_market'));
 
 $url=replace_canshu($app['appurl']);
 header("Location: ".$url);
