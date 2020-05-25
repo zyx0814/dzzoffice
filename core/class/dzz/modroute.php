db9ba22ef4879cc7db27c59148cfbfc2eb175ddd
zyx <zyx@dzz.cc>
2020-05-25 16:38:05 +0800

    修复user/login/logging.php报有木马的问题

--- core/class/dzz/modroute.php
+++ core/class/dzz/modroute.php
@@ -24,6 +24,7 @@ class Modroute{
 
             }
 			$params['route_file']=$file;
+			return $file;
         }
     }
 }
--- user/login/logging.php
+++ user/login/logging.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: a
- * Date: 2017/3/1
- * Time: 16:26
- */
+
 if(!defined('IN_DZZ')) {
     exit('Access Denied');
 }
 Hook::listen('mod_run');//执行配置
-Hook::listen('mod_start',$_GET);//模块路由
-include $_GET['route_file'];
+@include Hook::listen('mod_start',$_GET,null,true);//模块路由
+dexit();
