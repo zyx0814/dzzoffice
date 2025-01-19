a0cdb84dfa2836f19c0b2a5cbeaa658ebe8771b4
小胡 <3203164629@qq.com>
2025-01-19 21:28:33 +0800

    修复数据表格点击重置按钮后不按类型加载数据问题和添加刷新按钮

--- core/core_version.php
+++ core/core_version.php
@@ -13,5 +13,5 @@ if(!defined('IN_DZZ')) {
 if(!defined('CORE_VERSION')) {
 	define('CORE_VERSION', '2.3.2');
 	define('CORE_RELEASE', '20250101');
-	define('CORE_FIXBUG' , '21000000');
+	define('CORE_FIXBUG' , '23200000');
 }
