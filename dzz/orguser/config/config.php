f626efe81d9b1b9dfd6400307dff2646f58b15bd
小胡 <3203164629@qq.com>
2025-06-27 13:16:29 +0800

    修复模板调用路径问题，确保新代码生效。

--- dzz/orguser/config/config.php
+++ dzz/orguser/config/config.php
@@ -2,6 +2,6 @@
 return array(
     'allow_robot' => false,
     'about' => array('name_en' => '机构用户',
-        'version' => 'V2.0'),
+        'version' => 'V2.01'),
     'libfile' => 'function/appperm',
 );
