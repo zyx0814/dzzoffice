48d879dd10d13715aa6237bbd4f341951cdce54a
小胡 <3203164629@qq.com>
2025-06-28 10:34:14 +0800

    更新

--- dzz/orguser/config/config.php
+++ dzz/orguser/config/config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
     'allow_robot' => false,
+    'template' => 1,
     'about' => array('name_en' => '机构用户',
         'version' => 'V2.01'),
     'libfile' => 'function/appperm',
--- dzz/share/config/config.php
+++ dzz/share/config/config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
     'allow_robot' => false,
+    'template' => 1,
     'about' => array('name_en' => '分享管理',
         'version' => 'V2.0'),
     'libfile' => 'function/appperm',
