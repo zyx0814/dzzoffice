b6f7d9e44c675737979d28f71b164de8e97fa41e
小胡 <3203164629@qq.com>
2025-10-25 15:29:12 +0800

    修复pdf阅读器插件只能打印一页内容问题

--- dzz/pdf/index.php
+++ dzz/pdf/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 当前版本: 2.5.207 2020/06/01
+ * 当前版本: 5.3.31
  * 版本更新: https://github.com/mozilla/pdf.js/releases
  * 搜索:    var userOptions = Object.create(null);
  * 添加:    userOptions =  window.pdfOptions || userOptions;
@@ -15,15 +15,20 @@ $meta = IO::getMeta($path);
 if (!$meta) showmessage('file_not_exist');
 if($meta['error']) showmessage($meta['error']);
 $perm_download = 1;
-$perm_print = 1;
 if ($meta['rid']) {
     if (!perm_check::checkperm('read', $meta)) showmessage('file_read_no_privilege', dreferer());
     if (!perm_check::checkperm('download', $meta)) {
         $perm_download = 0;
-        $perm_print = 0;
     }
 }
 $file = $_G['siteurl'] . 'index.php?mod=io&op=getStream&path=' . $_GET['path'] . '&filename=' . $meta['name'];
 $navtitle = $meta['name'];
+if($_G['language']=='zh-cn'){
+    $lang = 'zh-CN';
+}elseif($_G['language']=='en-us'){
+    $lang = 'en-US';
+} else{
+    $lang = $_G['language'];
+}
 include template('viewer');
 exit();
