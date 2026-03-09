36605b07643d4d3e2808e935a7208c5f8d3a87d2
小胡 <3203164629@qq.com>
2026-03-09 14:23:10 +0800

    优化默认首页的日期时间显示

--- dzz/index/index.php
+++ dzz/index/index.php
@@ -55,7 +55,57 @@ if ($do == 'saveIndex') {
             array_multisort($arrSort[$sort['field']], constant($sort['direction']), $applist_1);
         }
     }
-    $servertime = time() * 1000;
-    $regdatedays = floor((time() - intval($_G['member']['regdate'])) / (60 * 60 * 24));
+    $timestamp = time();
+    $servertime = $timestamp * 1000;
+    $regdatedays = human_time(intval($_G['member']['regdate']));
+    $hour = date('H', $timestamp);
+
+    // 计算问候语
+    if ($hour < 6) {
+        $greeting = '凌晨好！';
+    } elseif ($hour < 9) {
+        $greeting = '早上好！';
+    } elseif ($hour < 12) {
+        $greeting = '上午好！';
+    } elseif ($hour < 14) {
+        $greeting = '中午好！';
+    } elseif ($hour < 18) {
+        $greeting = '下午好！';
+    } elseif ($hour < 22) {
+        $greeting = '晚上好！';
+    } else {
+        $greeting = '夜深了！';
+    }
+
+    // 格式化日期和时间
+    $currentDate = date('Y年n月j日', $timestamp);
+    $weekdayMap = ['日', '一', '二', '三', '四', '五', '六'];
+    $weekday = '星期' . $weekdayMap[date('w', $timestamp)];
+    $currentTime = date('H:i:s', $timestamp);
     include template('main');
+}
+/**
+ * 精准人性化加入时长（自动识别闰年、大小月）
+ * @param int $time 加入时间戳
+ * @return string
+ */
+function human_time($time)
+{
+    try {
+        // 时间戳转 DateTime（支持 int 时间戳）
+        $join = new DateTime();
+        $join->setTimestamp($time);
+        
+        $now = new DateTime();
+        $diff = $join->diff($now);
+
+        $result = '';
+        if ($diff->y > 0) $result .= $diff->y . '年';
+        if ($diff->m > 0) $result .= $diff->m . '个月';
+        if ($diff->d > 0) $result .= $diff->d . '天';
+
+        return $result ?: '今天加入';
+    } catch (Exception $e) {
+        return '时间异常';
+    }
 }
